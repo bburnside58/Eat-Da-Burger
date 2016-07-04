50fbfe9b2fb6a6c2f2c43f34ae81f70e25d7eeee
Brian Burnside <bcburnside58@gmail.com>
2016-07-04 15:37:34 -0400

    half way through handlebars

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,6 +3,23 @@ var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 
-var router = {
-	
+// LOAD DATA
+// We are linking our routes to a series of "data" sources. 
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var callOrmFunctions = require('../models/burger.js');
+//burgers.js
+
+
+
+module.exports = function(app){
+	app.get('/', function(req, res) {
+    //connection.query('SELECT * FROM quotes;', function(err, data) {
+        //if (err) throw err;
+        res.render('index', {
+            burgers: data
+        });
+    });
+//});
 };
--- server.js
+++ server.js
@@ -1,3 +1,37 @@
 var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
+var app = express();
+
+var PORT = 8080;
+
+// BodyParser makes it easy for our server to interpret data sent to it.
+// The code below is pretty standard.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+//Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + '/public'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'))
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+    	defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+
+require('./controllers/burgers_controller.js')(app); 
+
+
+app.listen(PORT, function() {
+    console.log("Listening on PORT %d", PORT);
+});
